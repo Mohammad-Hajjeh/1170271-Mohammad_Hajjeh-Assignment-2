4e73c7082ea12652d345f5c50854fbc6e9d73d51
Mohammad-Hajjeh <mohammadhaja1999@gmail.com>
2021-04-21 11:57:29 +0300

    Version 1.0

--- app/src/main/java/edu/bzu/a1170271_mohammadhajjeh_assignment2/FirstActivity.java
+++ app/src/main/java/edu/bzu/a1170271_mohammadhajjeh_assignment2/FirstActivity.java
@@ -2,7 +2,9 @@ package edu.bzu.a1170271_mohammadhajjeh_assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+
+    public void creatCv(View view) {
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void exitCv(View view) {
+        System.exit(0);
     }
 }
